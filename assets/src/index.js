83b8134c0320cc8478b694314e0c594b2184c461
joshuabnntt <joshua.bennett@smartcop.com>
2020-08-10 09:02:05 -0500

    Initial Commit

---
+++ assets/src/index.js
@@ -0,0 +1,5 @@
+import alpine from 'alpinejs'
+import alpineturbolinksadapter from 'alpine-turbolinks-adapter'
+import turbolinks from 'turbolinks'
+
+turbolinks.start()
---
+++ assets/tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+    purge: ['./**/*.css', '../**/*.html'],
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  }
